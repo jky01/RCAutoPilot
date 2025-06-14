0dc0cdf67de89ad859018ab14b8fb4ba4f8eea6f
jky01 <jky01@hotmail.com>
2025-06-14 10:40:40 +0800

    Fix CLRNet install cleanup

--- setup_software_noroot.sh
+++ setup_software_noroot.sh
@@ -18,8 +18,9 @@ sed -i \
   -e 's/^sklearn$/scikit-learn/' \
   -e 's/^Shapely==.*/Shapely/' \
   CLRNet/requirements.txt
-# Remove any stale egg-info directories from previous installs
-find CLRNet -maxdepth 1 -name '*.egg-info' -exec rm -rf {} +
+# Remove any stale build or egg-info directories from previous installs
+find CLRNet -name '*.egg-info' -exec rm -rf {} +
+find CLRNet -name 'build' -type d -exec rm -rf {} +
 # Skip building CLRNet's CUDA extension which fails with modern toolchains
 sed -i \
   -e 's/from torch.utils.cpp_extension import CUDAExtension, BuildExtension/from torch.utils.cpp_extension import BuildExtension/' \
